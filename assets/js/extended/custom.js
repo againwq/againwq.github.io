d5550f46c0f3d6782b67a2f0297896da0ac91acc
againwq <againwq@users.noreply.github.com>
2024-09-04 17:00:31 +0000

    post：C使用泰勒展开实现sinx againwq/hugo_blog@d9da27c9f1ec82be139a8b7884700fdb58ad110a

--- assets/js/extended/custom.js
+++ assets/js/extended/custom.js
@@ -1,4 +1,4 @@
-(()=>{var e,n,s=Object.defineProperty,o=(e,t)=>{for(var n in t)s(e,n,{get:t[n],enumerable:!0})},t={};o(t,{custom_param:()=>e,default:()=>n}),e={domain:"againdev.top",name:"test"},n={custom_param:e},myFun(15),console.log("params :>> ",t)})()
+(()=>{var e,n,s=Object.defineProperty,o=(e,t)=>{for(var n in t)s(e,n,{get:t[n],enumerable:!0})},t={};o(t,{custom_param:()=>e,default:()=>n}),e={domain:"againwq.github.io",name:"test"},n={custom_param:e},myFun(15),console.log("params :>> ",t)})()
 ;
 function myFun(count){
     for (let index = 0; index < count; index++) {
